5486d59c33df722489f813a71b1bfbe6390ad2ee
Jonggyu Park <jonggyu@skku.edu>
2020-09-14 17:11:05 +0900

    FragPicker v1

---
+++ defrag_dir_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+walk_dir () {
+	shopt -s nullglob dotglob
+
+	for pathname in "$1"/*; do
+		if [ -d "$pathname" ]; then
+			walk_dir "$pathname"
+		else
+#			echo "$pathname"
+#			python3.8 /home/jonggyu/Research/Benchmarks/HotStorage/defrag.py $pathname 128
+			python3.8 /home/jonggyu/Research/Benchmarks/DATE/iozone/defrag_all.py $pathname 128
+
+		fi
+	done
+}
+
+
+walk_dir "/mnt"
