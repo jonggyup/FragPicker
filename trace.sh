a72d8f621bce9bc57b7a0fbef49ced65a3655d82
Jonggyu Park <jonggyu@skku.edu>
2020-10-19 18:04:23 +0900

    Added a new tracer

---
+++ trace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#$1 is process name
+/usr/share/bcc/tools/trace 'vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos) "ino = %llu | size = %llu | pos = %u", file->f_inode->i_ino, count, *pos' -n $1
