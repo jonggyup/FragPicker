3587dc70195d02a945198d58dc42e058e864da13
Jonggyu Park <jonggyu@skku.edu>
2020-10-20 18:38:55 +0900

    Post-processing: 1. Parsing and Deciding the read range. 2. Sorting by the start offset

--- traces/processing.py
+++ traces/processing.py
@@ -7,16 +7,26 @@ result_file = open("./after_process.result", "w+")
 trace_file = open("./trace.result", "rb+",0)
 
 lines = trace_file.readlines()
-
+file_list = set()
 for line in lines[:-1]:
     req_info = line.split() #req_info = [i_no, size, pos]
     #filename = subprocess.call(["find", "/mnt", "-inum", req_info[0]])
     start = int(req_info[2])
     end = int(req_info[2]) + int(req_info[1]) - 1
-    print(str(start) ,str(end), file=open("./"+str(req_info[0].decode('utf-8'))+".txt", 'a+'))
+    print(str(start) ,str(end), "1", file=open("./"+str(req_info[0].decode('utf-8'))+".txt", 'a+'))
+    file_list.add(req_info[0])
 
-    #print(str(req_info[0]))
- 
+
+for filename in file_list:
+    filename = filename.decode("utf-8")
+    subprocess.call(["sort", "-g", "./"+str(filename)+".txt"], stdout=open("./tmp.txt", "w+"))
+    subprocess.call(["mv", "./tmp.txt", "./"+str(filename)+".txt"])
+
+
+
+
+
+
 
 
 result_file.close()
