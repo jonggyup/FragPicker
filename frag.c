314b3b524c80b174f64264fabad4188e031cc2da
Jonggyu Park <jonggyu@skku.edu>
2020-10-26 22:45:13 +0900

    The prototype of Fragmentor is complete.

--- frag.c
+++ frag.c
@@ -12,16 +12,17 @@
 
 int main()
 {
-	int fd;
+	int fd, fd2, fd3;
 	int ret;
 	unsigned char *buf;
-	ret = posix_memalign((void **)&buf, 512, BUF_SIZE);
+	ret = posix_memalign((void **)&buf, 4096, BUF_SIZE);
 	if (ret) {
 		perror("posix_memalign failed");
 		exit(1);
 	}
-
-	fd = open("/mnt/b.c", O_RDWR | O_DIRECT, 0755);
+	fd = open("/mnt/a.c", O_RDWR | O_DIRECT | O_SYNC | O_CREAT, 0755);
+	fd2 = open("/mnt/tmp", O_RDWR | O_DIRECT | O_SYNC | O_CREAT, 0755);
+	fd3 = open("/mnt/dummy", O_RDWR | O_DIRECT | O_SYNC | O_CREAT, 0755);
 	if (fd < 0){
 		perror("open failed");
 		exit(1);
@@ -29,15 +30,24 @@ int main()
 
 	do {
 		ret = read(fd, buf, BUF_SIZE);
-		if (ret < 0) {
+		if (ret == 0)
+			break;
+		if (ret < 0) 
+			perror("read ./direct_io.data failed");
+		ret = write(fd2, buf, ret);
+		write(fd3, buf, ret);
+		printf("%d\n",ret);
+		if (ret < 0) 
 			perror("write ./direct_io.data failed");
-		lseek(fd, -1*ret, 0);
-		write(1, buf, ret);
-		}
 	} while (ret > 0);
+	
+	printf("finished \n");
+	close(fd);
+	close(fd2);
+
+	rename("/mnt/tmp", "/mnt/a.c");
 
 	free(buf);
-	close(fd);
 }
 
 
