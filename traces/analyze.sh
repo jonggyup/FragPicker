a9ad1adb8d37e9f859612c491140d3b385307b64
Jonggyu Park <jonggyu@skku.edu>
2020-10-27 19:38:28 +0900

    ycsb running script is added. Replaying ycsb traces is still unsolved.

--- traces/analyze.sh
+++ traces/analyze.sh
@@ -10,21 +10,21 @@ sleep 5
 #sh -c './trace.sh iozone | tee trace.result' &
 id=$!
 #run benchmark here
-iozone -i 1 -r 128k -f /mnt/2 -w -I -s 10m -+n
+#iozone -i 1 -r 128k -f /mnt/2 -w -I -s 10m -+n
 #iozone -i 5 -r 128k -j 4 -f /mnt/2 -w -I -s 100m -+n
 #grep -r "asdf" /mnt/2
 #fio --directory=/mnt --name fio_test_file --direct=1 --rw=randread --bs=128k --size=1G --numjobs=1 --time_based --runtime=30 --group_reporting --norandommap 
 
 #YCSB benchmark
-#path=/home/jonggyu/Research/ATC2021/Evaluation/ATC/YCSB
+path=/home/jonggyu/Research/ATC2021/Evaluation/ATC/YCSB
 #(cd $path && ./bin/ycsb run rocksdb -s -P workloads/workloadc -p rocksdb.dir=/mnt/ycsb-rocksdb-data) &> /dev/null
-#(cd $path && ./bin/ycsb run mongodb -s -P workloads/workloadc)
+(cd $path && ./bin/ycsb run mongodb -s -P workloads/workloadc)
 
 #MongoDB
-path=/home/jonggyu/Research/ATC2021/Evaluation/mongodb
+#path=/home/jonggyu/Research/ATC2021/Evaluation/mongodb
 #(cd $path && ./replay.sh)
 
-#sleep 100
+sleep 100
 kill -INT $id
 sleep 5
 kill $(pgrep trace)
