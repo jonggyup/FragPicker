5ed30b54f1de09fb510c56f1be09c31510110ae4
Jonggyu Park <jonggyu@skku.edu>
2021-08-19 18:07:54 +0900

    c codes for seq read/write

---
+++ evaluation/Motivation/write_seq.c
@@ -0,0 +1,59 @@
+#define _GNU_SOURCE     /* Obtain O_DIRECT definition from <fcntl.h> */
+#include <fcntl.h>
+#include <malloc.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
+	
+int
+main(int argc, char *argv[])
+{
+	int fd;
+	ssize_t numWrite = 0;
+	ssize_t fileSize = 0;
+	size_t size;
+	off_t offset;
+	unsigned char *buf;
+	int bufSize=0;
+	clock_t t;
+	struct timeval start, end;
+	long secs_used, micros_used;
+
+	size = 1024*atoi(argv[2]);
+	offset = 0;
+	bufSize = posix_memalign((void **)&buf, 4096, size);
+	memset(buf, 0, bufSize);
+
+	fd = open(argv[1], O_RDWR | O_DIRECT);
+	if (fd == -1)
+		printf("open error\n");
+
+	/* memalign() allocates a block of memory aligned on an address that
+	 * is a multiple of its first argument. By specifying this argument as
+	 * 2 * 'alignment' and then adding 'alignment' to the returned pointer,
+	 * we ensure that 'buf' is aligned on a non-power-of-two multiple of
+	 * 'alignment'. We do this to ensure that if, for example, we ask
+	 * for a 256-byte aligned buffer, we don't accidentally get
+	 * a buffer that is also aligned on a 512-byte boundary. */
+	fileSize = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
+	gettimeofday(&start, NULL);
+	while (numWrite < fileSize) {
+		numWrite += write(fd, buf, size);
+		
+		if (numWrite == -1)
+			printf("write error\n");
+
+		if (numWrite >= fileSize)
+			break;
+
+	}
+	gettimeofday(&end, NULL);
+	double elapsed_time = ((double)t)/CLOCKS_PER_SEC;
+
+	secs_used=(end.tv_sec - start.tv_sec);
+	micros_used = secs_used*1000000 + end.tv_usec - start.tv_usec;
+
+	printf("Throughput = %f\n", (double)numWrite/micros_used);
+
+}
